1e3418e9671727540bf7a20d38707f4e2669dfc4
Gustavo Beserra <96631157+GustavoBGomes@users.noreply.github.com>
2023-11-26 14:05:15 -0300

    Sequência de Fibonacci

---
+++ Sequencia_de_Fibonacci/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int n_1 = 1;
+int n_2 = 0;
+int f = 0;
+
+Console.WriteLine("Sequência de Fibonacci");
+
+for (var i = 1; i <= 20; i++) {
+
+   f = n_1 + n_2;
+   n_2 = n_1;
+   n_1 = f;
+
+    Console.WriteLine($"A Sequência é: {f}");
+
+}
+
+Console.ReadKey();
