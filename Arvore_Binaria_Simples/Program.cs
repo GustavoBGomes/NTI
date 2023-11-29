ea91506d720cc41854c73471fa9919b1ce61f685
Gustavo Beserra <96631157+GustavoBGomes@users.noreply.github.com>
2023-11-28 21:58:34 -0300

    Árvore Binária Simples

---
+++ Arvore_Binaria_Simples/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class No
+{
+    public int data;
+    public No esquerda, direita;
+
+    public No(int value)
+    {
+        data = value;
+        esquerda = direita = null;
+    }
+}
+
+class Arvore
+{
+    // Raiz da árvore binária
+    public No raiz;
+
+    public Arvore()
+    {
+        raiz = null;
+    }
+
+    public void InOrderTraversal(No no)
+    {
+        if (no == null)
+            return;
+
+        InOrderTraversal(no.esquerda);
+
+        Console.Write(no.data + " ");
+
+        InOrderTraversal(no.direita);
+    }
+
+    public void PrintInOrder()
+    {
+        InOrderTraversal(raiz);
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Arvore arvore = new Arvore();
+        arvore.raiz = new No(4);
+        arvore.raiz.esquerda = new No(2);
+        arvore.raiz.direita = new No(5);
+        arvore.raiz.esquerda.esquerda = new No(1);
+        arvore.raiz.esquerda.direita = new No(3);
+
+        Console.WriteLine("Valores da árvore em ordem:");
+        arvore.PrintInOrder();
+    }
+}
